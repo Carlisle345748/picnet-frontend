319df8e8b62e705c8fcefbc8f68a33b4aa42fff7
Zikun Cui <czk86606733@gmai.com>
2023-01-16 02:01:42 -0800

    Install dependence

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,28 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/graphql': {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true
+      },
+      '^/media/.*': {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true
+      }
+    }
+  },
+  preview: {
+    proxy: {
+      '/graphql': {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true
+      },
+      '^/media/.*': {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true
+      }
+    }
+  }
 })

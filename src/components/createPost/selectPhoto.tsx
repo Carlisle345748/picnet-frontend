5368905b5d255154c568cfaaee221da88550e997
Zikun Cui <czk86606733@gmai.com>
2023-01-18 12:59:55 -0800

    Support drag and drop upload photo

--- src/components/createPost/selectPhoto.tsx
+++ src/components/createPost/selectPhoto.tsx
@@ -2,16 +2,50 @@ import {Box, CardMedia, Stack, Typography, useTheme} from "@mui/material";
 import FileUploadIcon from "@mui/icons-material/FileUpload";
 import ErrorIcon from '@mui/icons-material/Error';
 import {useFormContext, useWatch} from "react-hook-form";
+import {DragEventHandler, useState} from "react";
 
 
 export const PhotoPlaceHolder = () => {
-    const {formState: {errors} } = useFormContext();
+    const {formState: {errors}, setValue} = useFormContext();
     const theme = useTheme();
+    const [dragActive, setDragActive] = useState(false);
+
+    const handleDrag: DragEventHandler = async (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        if (e.type === "dragenter" || e.type == "dragover") {
+            setDragActive(true);
+        } else if (e.type === "dragleave") {
+            setDragActive(false);
+        }
+    }
+
+    const handleDrop: DragEventHandler = async (e) => {
+        e.preventDefault();
+        setValue("photo", e.dataTransfer.files);
+    }
+
+    const getBgColor = () => {
+        if (dragActive) return "#e9f3fd";
+        if (errors.photo) return "#fcd6d6";
+        return theme.palette.grey['300'];
+    }
+
+    const getBorder = () => {
+        if (dragActive) return "2px solid #3463ff";
+        if (errors.photo) return "2px solid red";
+        return "2px solid transparent";
+    }
+
     return (
         <Stack
+            onDragEnter={handleDrag}
+            onDragLeave={handleDrag}
+            onDragOver={handleDrag}
+            onDrop={handleDrop}
             sx={{
-                backgroundColor: errors.photo ? "#fcd6d6": theme.palette.grey['300'],
-                border: errors.photo ? "2px solid red": "",
+                backgroundColor: getBgColor(),
+                border: getBorder(),
                 width: 480,
                 height: 600,
                 alignItems: 'center',
@@ -37,17 +71,33 @@ export const PhotoPlaceHolder = () => {
                         ? <ErrorIcon fontSize='large' color='error'/>
                         : <FileUploadIcon fontSize='large' color='action'/>
                 }
-                <Typography color={errors.photo ? "#d32f2f": "textPrimary"}>
-                    {errors.photo ? errors.photo.message as string : "Click to upload photo"}
+                <Typography color={errors.photo ? "#d32f2f" : "textPrimary"}>
+                    {errors.photo ? errors.photo.message as string : "Drag and drop or click to upload"}
                 </Typography>
             </Stack>
         </Stack>
     )
 }
 
-export const Photo = ({photo}: {photo: File}) => {
+export const Photo = ({photo}: { photo: File }) => {
+    const {setValue} = useFormContext();
+
+    const handleDrag: DragEventHandler = async (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    const handleDrop: DragEventHandler = async (e) => {
+        e.preventDefault();
+        setValue("photo", e.dataTransfer.files);
+    }
+
     return (
         <Stack
+            onDragEnter={handleDrag}
+            onDragLeave={handleDrag}
+            onDragOver={handleDrag}
+            onDrop={handleDrop}
             alignItems="center"
             justifyContent="center"
             height="100%"
@@ -63,17 +113,20 @@ export const Photo = ({photo}: {photo: File}) => {
                 }
             }}
         >
-            <CardMedia
-                sx={{maxHeight: "100%", objectFit: "contain"}}
-                component="img"
-                image={URL.createObjectURL(photo)}
-            />
+            {
+                photo &&
+                <CardMedia
+                    sx={{maxHeight: "100%", objectFit: "contain"}}
+                    component="img"
+                    image={URL.createObjectURL(photo)}
+                />
+            }
         </Stack>
     )
 }
 
 export const SelectPhoto = () => {
-    const { register } = useFormContext();
+    const {register} = useFormContext();
     const photo = useWatch({name: 'photo', defaultValue: null});
 
     return (
